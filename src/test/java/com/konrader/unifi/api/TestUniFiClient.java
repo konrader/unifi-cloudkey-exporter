1272272b58d478012c8c2931266ab8eb177cb7f4
Konrad Eriksson <konrad@konraderiksson.com>
2019-08-27 10:13:06 +0200

    Fix correct amount of constrcutor args for test class

--- src/test/java/com/konrader/unifi/api/TestUniFiClient.java
+++ src/test/java/com/konrader/unifi/api/TestUniFiClient.java
@@ -5,10 +5,10 @@ import java.io.IOException;
 public class TestUniFiClient {
 
 	public static void main(String[] args) throws IOException {
-		UniFiManagementPortal ump = new UniFiManagementPortal(args[0]);
+		UniFiManagementPortal ump = new UniFiManagementPortal(args[0], args[1], args[2]);
 		UMPInfo info = ump.getInfo();
-		for(UMPInfo.Service serv : info.data.services) {
-			System.out.println(" "+serv.name+" "+info.data.hostname+":"+serv.port);
+		for (UMPInfo.Service serv : info.data.services) {
+			System.out.println(" " + serv.name + " " + info.data.hostname + ":" + serv.port);
 		}
 	}
 
